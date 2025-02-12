5059b1eced75125aef700a8aad2e729ad6ea2e7e
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2025-02-12 23:39:01 +0100

    update and fetch single and all users successfully

--- controller/userController.js
+++ controller/userController.js
@@ -28,3 +28,46 @@ export const createUser = async (req, res) => {
     data: newUser,
   });
 };
+
+//update
+export const updateUser = async (req, res) => {
+  const userId = req.params.id;
+  const { name, email, password } = req.body;
+
+  await prisma.user.update({
+    where: {
+      id: Number(userId),
+    },
+    data: {
+      name,
+      email,
+      password,
+    },
+  });
+
+  return res.json({ status: 200, message: "User updated successfully" });
+};
+
+//fetch users
+export const fetchUsers = async (req, res) => {
+  const users = await prisma.user.findMany({});
+
+  return res.json({
+    status: 201,
+    message: "Users fetched successfully",
+    data: users,
+  });
+};
+
+//get single user by id
+export const singleUser = async (req, res) => {
+  const userId = req.params.id;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: Number(userId),
+    },
+  });
+
+  return res.json({ status: 200, data: user, message: "Fetched single user" });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { createUser } from "../controller/userController.js";
+import {
+  createUser,
+  fetchUsers,
+  singleUser,
+  updateUser,
+} from "../controller/userController.js";
 
 const router = Router();
 
 router.post("/create", createUser);
+router.put("/update/:id", updateUser);
+router.get("/getAll", fetchUsers);
+router.get("/get/:id", singleUser);
 
 export default router;
